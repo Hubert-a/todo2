aca0f9f0f3946a2e3aaea0c3ce45a84f0c105890
Hubert-a <hubertamarfio@gmail.com>
2021-11-02 17:38:53 +0000

    update

---
+++ sidebar.php
@@ -0,0 +1,70 @@
+<!DOCTYPE>
+<html lang="en">
+    <head>
+        <title>sidebar</title>
+        <link rel="stylesheet" href="sidebar.css">
+        <link rel="stylesheet" href="./assets/fontawesome/css/all.min.css">
+        <link rel="stylesheet" href="./assets/css/dashboard.css">
+
+
+    </head>
+    <body>
+        <div class="sidebar">
+            <div class="logo_content">
+                <div class="logo">
+                    <div class="logo_name">TODO</div>
+                </div>
+                <i class="fas fa-ellipsis-h" id="btn"></i>
+            </div>
+            <ul class="nav_list">
+                <li>
+                    <a href="#">
+                    <i class="fas fa-tachometer-alt"></i>
+                    <span class="links_name">Dashboard</span>
+                    </a>
+                    <!-- <span class="tooltip">Dashboard</span> -->
+                </li>
+                <li>
+                    <a href="#">
+                    <i class="fas fa-plus"></i>
+                    <span class="links_name">Create New Tasks</span>
+                    </a>
+                    <!-- <span class="tooltip">Dashboard</span> -->
+                </li>
+                <li>
+                    <a href="#">
+                    <i class="fas fa-plus"></i>
+                    <span class="links_name">Add a Collection</span>
+                    </a>
+                    <!-- <span class="tooltip">Dashboard</span> -->
+                </li>
+                <li>
+                    <a href="#">
+                    <i class="fas fa-user"></i>
+                    <span class="links_name">View Profile</span>
+                    </a>
+                    <!-- <span class="tooltip">Dashboard</span> -->
+                </li>
+                <li>
+                    <a href="#">
+                    <i class="fas fa-cog"></i>
+                    <span class="links_name">Settings</span>
+                    </a>
+                    <!-- <span class="tooltip">Dashboard</span> -->
+                </li>
+            </ul>
+            <div class="profile_content">
+                <div class="profile">
+                    <div class="profile_details">
+                        <img src="./assets/images/undraw_profile_pic_ic5t-removebg-preview.png" alt="profile" class="profile">
+                        <div class="name_job">
+                            <div class="name">Hubert Amarfio</div>
+                            <div class="job">Web Developer, Union Systems Global</div>
+                        </div>
+                    </div>
+                    <i class="fas fa-sign-out-alt"></i> 
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
